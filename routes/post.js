a5624544244b099874486d2e124afc0d5082c852
Nihar277 <niharbharatia614@gmail.com>
2024-07-15 17:04:18 +0530

    project working

---
+++ routes/post.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const postSchema = mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+        },
+
+        posttitle: {
+            type: String,
+            required: true,
+        },
+        postdescription: {
+            type: String,
+            required: true,
+        },
+        postimage:{
+            type:String,
+            required:true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        }
+    }
+)
+
+module.exports = mongoose.model('Post', postSchema)
