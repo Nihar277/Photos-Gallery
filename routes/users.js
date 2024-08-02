97dc6dd1d1467939856f7ac42d4c244679bb3b06
Nihar277 <niharbharatia614@gmail.com>
2024-08-02 23:13:25 +0530

    change in db

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const plm = require('passport-local-mongoose')
 
 mongoose.connect("mongodb+srv://whitehat2774:CZuB9d0b3ksJHz2x@cluster0.sjpnq7x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/gallery")
+// mongoose.connect("mongodb://localhost:27017/Gallery")
 const userSchema = mongoose.Schema({
   username:String,
   name:String,
